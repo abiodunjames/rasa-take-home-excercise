b918955a19617735dc1a0b29c16bdfea33045282
samueljames <samuel4abiodun@gmail.com>
2020-08-07 12:27:57 +0200

    Initial project setup

---
+++ app/main/seed/user.py
@@ -0,0 +1,18 @@
+from app.main.model.user import User
+from app.main import db
+import datetime
+
+SEED_ADMIN_EMAIL = 'admin@test.com'
+SEED_ADMIN_PASSWORD= '123456'
+def seed_admin_user():
+    """
+    Seed initial admin user. This user will be use to create other non-admin user.
+    """
+    user = User(
+            email=SEED_ADMIN_EMAIL,
+            password=SEED_ADMIN_PASSWORD,
+            registered_on=datetime.datetime.utcnow(),
+            admin=True
+        )
+    db.session.add(user)
+    db.session.commit()
---
+++ app/main/service/user_service.py
@@ -0,0 +1,35 @@
+import uuid
+import datetime
+from app.main import db
+from app.main.model.user import User
+
+def save_new_user(data):
+    user = User.query.filter_by(email=data['email']).first()
+    if not user:
+        new_user = User(
+            email=data['email'],
+            username=data['username'],
+            password=data['password'],
+            registered_on=datetime.datetime.utcnow()
+        )
+        save_changes(new_user)
+        response_object = {
+            'status': 'success',
+            'message': 'Successfully registered.'
+        }
+        return response_object, 201
+    else:
+        response_object = {
+            'status': 'fail',
+            'message': 'User already exists. Please Log in.',
+        }
+        return response_object, 409
+
+
+def get_all_users():
+    return User.query.all()
+
+
+def save_changes(data):
+    db.session.add(data)
+    db.session.commit()
